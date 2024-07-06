11e5070bea77019358d78badb3ad5035226f224c
lolfser <martin-keil-martin@web.de>
2024-07-06 12:20:06 +0200

    Remove unused import

--- app/Models/Programblockmetas.php
+++ app/Models/Programblockmetas.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
