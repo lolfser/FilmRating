218f65210bce04741530f382d9004f7d915198f3
lolfser <martin-keil-martin@web.de>
2025-06-27 18:26:30 +0200

    Fix dateString on model day

--- app/Http/Controllers/ProgramblocksController.php
+++ app/Http/Controllers/ProgramblocksController.php
@@ -30,13 +30,14 @@ class ProgramblocksController extends Controller {
         foreach ($metas as $meta) {
             $meta->location;
             if ($meta->day !== null) {
-                $meta->day->dateString = (new \DateTime($meta->day->date))->format('l, d.m.Y');
-                $meta->day->dateString .= ', ';
-                $meta->day->dateString .= $meta->start;
-                $meta->day->dateString .= ' - ';
+                $dateString = (new \DateTime($meta->day->date))->format('l, d.m.Y');
+                $dateString .= ', ';
+                $dateString .= $meta->start;
+                $dateString .= ' - ';
                 $dateTime = new \DateTime($meta->start ?? '');
                 $dateTime->add(new \DateInterval('PT' . ($meta->total_length ?? 0) . 'M'));
-                $meta->day->dateString .= $dateTime->format('H:i');
+                $dateString .= $dateTime->format('H:i');
+                $meta->day->setDateStringAttribute($dateString);
             }
             foreach (Programblocks::where('programblockmetas_id', $meta->id)->get() as $block) {
                 /** @var Programblocks $block */
